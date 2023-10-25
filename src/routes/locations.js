b81699d6fa91e812be491e0a3074e36ba2ac11cc
Mauro Núñez Cioci <mauroncioci@gmail.com>
2023-10-25 16:20:30 -0300

    delete admin permission to get locations

--- src/routes/locations.js
+++ src/routes/locations.js
@@ -3,7 +3,7 @@ const authRouter = require('express').Router();
 const { getLocations, createLocation, deleteLocation, modifyLocation } = require('../controllers/locations');
 const { isAdmin } = require('../controllers/auth');
 
-authRouter.get('/get', isAdmin, getLocations);
+authRouter.get('/get', getLocations);
 authRouter.post('/create', isAdmin, createLocation);
 authRouter.delete('/delete/:id', isAdmin, deleteLocation);
 authRouter.put('/modify/:id', isAdmin, modifyLocation);
