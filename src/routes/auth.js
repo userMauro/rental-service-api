32f2daff5f05c60985c73c0f515f08ea62f93682
usermauro <mauroncioci@gmail.com>
2023-10-27 20:20:35 -0300

    add list users and list products

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,8 @@
 const authRouter = require('express').Router();
 
-const { register, login, resetUserPassword, isAdmin } = require('../controllers/auth')
+const { register, login, resetUserPassword, isAdmin, getUsers } = require('../controllers/auth')
 
+authRouter.get('/users', isAdmin, getUsers)
 authRouter.post('/login', login)
 authRouter.post('/register', isAdmin, register)
 authRouter.put('/resetPass', isAdmin, resetUserPassword)
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,11 +1,12 @@
 const authRouter = require('express').Router();
 
-const { createProduct, scanProduct, transferProduct, getProductHistory } = require('../controllers/products');
+const { createProduct, scanProduct, transferProduct, getProductHistory, getAllProducts } = require('../controllers/products');
 const { isAdmin } = require('../controllers/auth');
 const { multarMiddleware } = require('../config/s3');
 
 authRouter.get('/scan/:barcode', scanProduct);
 authRouter.get('/history/:productId', isAdmin, getProductHistory);
+authRouter.get('/list', isAdmin, getAllProducts)
 authRouter.put('/transfer', multarMiddleware('image'), transferProduct);
 authRouter.post('/create', isAdmin, multarMiddleware('image'), createProduct);
 
